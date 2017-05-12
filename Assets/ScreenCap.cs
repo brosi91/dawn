d910ee555a0160a297edc4600efd5c21a011b11c
Rosina Brosi <rosina_b91@hotmail.com>
2017-05-12 17:29:25 +0200

    + screenshot-scene

---
+++ Assets/ScreenCap.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+// Robin Bornschein 2014
+
+
+/// <summary>
+/// Takes a Screenshot off of the MainCamera. Screenshots can be supersized and put in custom subDirectories.
+/// Non-existing directories will automatically be created.
+/// </summary>
+public static class ScreenCap {
+
+	public static void Take() {
+		Take(string.Empty,1);
+	}
+	public static void Take(int superSize) {
+		Take(string.Empty,superSize);
+	}
+	public static void Take(string subDirectory) {
+		Take(subDirectory,1);
+	}
+	
+	public static void Take(string subDirectory, int superSize) {
+		string dir = "Screenshots/";
+		string name;
+
+		// Create name based on given sub directory
+		if (subDirectory == string.Empty) name = "Screenshot_";
+		else name = subDirectory + "_";
+
+		// Create directory if necessairy
+		if (!Directory.Exists("Screenshots")) {
+			Directory.CreateDirectory("Screenshots");
+		}
+
+		// Create sub directory if necessairy
+		if (subDirectory != string.Empty) {
+			if (!Directory.Exists("Screenshots/" + subDirectory)) {
+				Directory.CreateDirectory("Screenshots/" + subDirectory);
+			}
+			dir += subDirectory + "/";
+		}
+
+		// Get screenshot number
+		int screenshotNumber = 0;
+		string filePath = dir + name + screenshotNumber.ToString() + ".png";
+		while (File.Exists(filePath)) {
+			screenshotNumber++;
+			filePath = dir + name + screenshotNumber.ToString() + ".png";	
+		}
+
+		// Make screenshot
+		Debug.Log("Capturing Screenshot. Path: " + filePath);
+		Application.CaptureScreenshot(filePath, superSize);
+	}
+
+}
---
+++ Assets/ScreenShotManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShotManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Tab)) {
+			ScreenCap.Take(6);
+		}
+	
+	}
+}
