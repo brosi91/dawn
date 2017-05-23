ba5760e16443b2123452446330948ecc9e45ba45
brosi91 <Rosina@Rosinas-MBP.fritz.box>
2017-05-23 17:49:51 +0200

    + assets + capsule colliders on assets + capsulecols on mushrooms & around lights

--- Assets/Scripts/Scr_CamInLight.cs
+++ Assets/Scripts/Scr_CamInLight.cs
@@ -6,12 +6,53 @@ public class Scr_CamInLight : MonoBehaviour {
 
 	public Scr_PlayerInput inputScr;
 	private MeshRenderer WaterRend;
-	
+	public LayerMask Mask;
 
-	void Awake(){
+	RaycastHit other;
+	bool On;
 
+	void FixedUpdate(){
+		if (Physics.SphereCast(transform.position, 0.5f, Vector3.up, out other, Mask)){
+			Debug.Log("Collision with"+ other.transform.name);
+			if (other.transform.tag == "Light"){
+				WaterRend = other.transform.GetComponentInChildren<MeshRenderer> ();
+				Debug.Log ("Entered into " + WaterRend.gameObject.name);
+				if (inputScr.Swim == false) {
+					WaterRend.enabled = false;
+				} else {
+					WaterRend.enabled = true;
+				}
+			}
+		} else {
+			if (WaterRend != null){
+				WaterRend.enabled = true;
+			}
+			WaterRend = null;
+		}
 	}
 
+	/*
+	void FixedUpdate(){
+		if (Physics.SphereCast(transform.position, 0.5f, Vector3.up, out other)){
+			if (other.transform.tag == "Light"){
+				WaterRend = other.transform.GetComponentInChildren<MeshRenderer> ();
+				if (On && inputScr.Swim == false) {
+					WaterRend.enabled = false;
+					On = false;
+				} else if (!On) {
+					WaterRend.enabled = true;
+					On = true;
+				}
+			}
+		} else if (!On) {
+			if (WaterRend != null){
+				WaterRend.enabled = true;
+			}
+			On = true;
+			WaterRend = null;
+		}
+	}*/
+	 /*
 	void OnTriggerStay (Collider other) {
 		if (other.tag == "Light") {
 			WaterRend = other.gameObject.GetComponentInChildren<MeshRenderer> ();
@@ -28,4 +69,5 @@ public class Scr_CamInLight : MonoBehaviour {
 			WaterRend.enabled = true;
 		}	
 	}
+	*/
 }
